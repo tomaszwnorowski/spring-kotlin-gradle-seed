8e5fa41e646e02a42b3b8b85d7f6015d3e6150d0
tomaszwnorowski <tomek.wnorowski@gmail.com>
2023-09-27 16:35:51 +0200

    Move @RequestMapping to top of resource controller

--- rest/src/main/kotlin/project/rest/ResourceRestController.kt
+++ rest/src/main/kotlin/project/rest/ResourceRestController.kt
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import project.module.api.CreateResourceCommand
 import project.module.api.ModuleApi
@@ -25,13 +26,14 @@ fun Resource.toRest(): RestResource =
     RestResource(id, name)
 
 @RestController
+@RequestMapping("/public/api/v1/resources")
 class ResourcesRestController(private val api: ModuleApi) {
 
-    @PostMapping("/public/api/v1/resources")
+    @PostMapping
     fun create(@RequestBody request: RestCreateResource): ResponseEntity<RestResource> =
         ResponseEntity.ok(api.save(request.toCommand()).toRest())
 
-    @GetMapping("/public/api/v1/resources/{id}")
+    @GetMapping("/{id}")
     fun get(@PathVariable id: String): ResponseEntity<RestResource> =
         ResponseEntity.ofNullable(api.findById(id)?.toRest())
 }
